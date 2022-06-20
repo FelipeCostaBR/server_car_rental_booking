a5c7a8f39c975286c35cf3bb65bd7addc5dd61a6
Felipe <felipejsilvacosta@gmail.com>
2022-06-20 10:54:41 +1000

    remove seeds

--- src/database/seeding/seeds/create-accounts.seed.ts
+++
@@ -1,8 +0,0 @@
-import { Factory, Seeder } from 'typeorm-seeding'
-import { Account } from '../../../modules/accounts/entities/Account'
-
-export default class CreateAccountDetail implements Seeder {
-  public async run(factory: Factory): Promise<void> {
-    await factory(Account)().createMany(10)
-  }
-}
